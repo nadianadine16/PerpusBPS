a7ea816cb53b8a5acc2aca345e33ba77be24d5f0
nadianadine16 <nadianadine012@gmail.com>
2021-01-08 19:44:50 +0700

    template header

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,5 +3,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
-
+    public function index(){
+        $this->load->view("template/header");
+        $this->load->view("template/footer");
+    }
 }
---
+++ application/views/template/footer.php
@@ -0,0 +1,25 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+
+<div class="footer-top">
+
+  <div class="container" data-aos="fade-up">
+
+    <div class="row  justify-content-center">
+      <div class="col-lg-6">
+        <h3>Perpustakaan BPS Kota Malang</h3>       
+      </div>
+    </div>
+
+    <div class="social-links">
+      <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+      <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+      <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+      <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+      <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+    </div>
+
+  </div>
+</div>
+</div>
+</footer><!-- End Footer -->

5a9de11c3f17bfb152db0dddf114027c2d6c2d1a
nadianadine16 <nadianadine012@gmail.com>
2021-01-08 20:51:07 +0700

    judul login

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Login Template</title>
+  <title>Login</title>
   <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
@@ -20,7 +20,7 @@
           </div>
           <div class="login-wrapper my-auto">
             <h1 class="login-title" style="width:500px">Log in Admin / Supervisor Perpustakaan</h1>
-            <form action="#!">
+            <form action="">
               <div class="form-group">
                 <label for="username">Username</label>
                 <input type="username" name="username" id="username" class="form-control" placeholder="Masukan Username Anda">
